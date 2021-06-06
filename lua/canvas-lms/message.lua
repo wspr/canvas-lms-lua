2e1fb967e8f4ec6ae289bf9e7bfdbd4499f18711
Will Robertson <wspr81@gmail.com>
2021-06-06 23:18:01 +0930

    improve msg printing and add some ldoc text

--- lua/canvas-lms/message.lua
+++ lua/canvas-lms/message.lua
@@ -3,7 +3,8 @@
 
 local canvas = {}
 
-canvas.message_group_wfile = function(self,send_check,msg)
+
+function canvas:message_group_wfile(send_check,msg)
 
   local function encode(str)
 		str = string.gsub (str, "\n", "\r\n")
@@ -42,8 +43,19 @@ canvas.message_group_wfile = function(self,send_check,msg)
 
 end
 
+--- Message user table arguments.
+-- The function `canvas:message_user` takes a table of arguments to define the message to send.
+-- The table may consist of the following
+-- @field canvasid The Canvas ID of the recipient (reqd)
+-- @field subject  The subject of the message to send
+-- @field body     The body text of the message to send
+-- @field course   The Canvas course ID to send from (defaults to defined course)
+-- @table @{message_user_args}
 
-canvas.message_user = function(self,send_check,msg)
+--- Message a specific Canvas user.
+-- @tparam bool send_check Toggle whether to truly send the message or just to pretty print it to the screen
+-- @tparam table msg table with entries to define message according to @{message_user_args}
+function canvas:message_user(send_check,msg)
 
   local function encode(str)
 		str = string.gsub (str, "\n", "\r\n")
@@ -71,7 +83,10 @@ canvas.message_user = function(self,send_check,msg)
   if send_check then
     self:post("conversations",opt)
   else
-    print(opt)
+    print("=========== FACSIMILE OF MESSAGE NOT SENT ===========")
+    print("Subject: "..msg.subject.."\n")
+    print("=====================================================")
+    print(msg.body)
   end
 
 
