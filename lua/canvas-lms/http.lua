14a5490d1e4655e083661db99cbb65cea0144ca9
Will Robertson <wspr81@gmail.com>
2021-06-13 21:46:50 +0930

    avoid luacheck warning

--- lua/canvas-lms/http.lua
+++ lua/canvas-lms/http.lua
@@ -100,13 +100,13 @@ function canvas:define_getter(field_name,index_name_arg,opt_default)
        arg[i] = v
     end
     local download = arg.download or false
-    if self_[field_name] == nil then
---      download = true
---[[
-this is not nuanced enough. first need to check if there is data cached through get_pages and only THEN force it if nothing available
-]]
-    end
     arg.download = nil
+--[[
+    -- this is not nuanced enough. first need to check if there is data cached through get_pages and only THEN force it if nothing available
+    if self_[field_name] == nil then
+      download = true
+    end
+--]]
     local opt = arg or {}
 
     print("# Getting "..field_name.." data currently in Canvas")
